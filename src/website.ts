1b2798a9e45f63f527776ee27f0179416cc6d90f
AmirHossein <109079832+SIZH3N@users.noreply.github.com>
2024-09-20 21:28:54 +0330

    check if the website script works
    adding a console.log

--- src/website.ts
+++ src/website.ts
@@ -1,5 +1,5 @@
 WA.chat.sendChatMessage('Hello world', 'Mr Robot');
-
+console.log("the website script is open now");
 
 WA.room.onEnterLayer('chat').subscribe(() => {
     WA.nav.openTab('https://www.wikipedia.org/');
