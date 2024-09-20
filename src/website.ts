9cd10d12b255f065c1c2c1e87dc6d7b68516bbd2
AmirHossein <109079832+SIZH3N@users.noreply.github.com>
2024-09-20 10:57:54 +0330

    adding website.ts
    to see if it's working

--- src/website.ts
+++ src/website.ts
@@ -1,7 +1,7 @@
 WA.chat.sendChatMessage('Hello world', 'Mr Robot');
 
 
-WA.room.onEnterLayer('website').subscribe(() => {
+WA.room.onEnterLayer('chat').subscribe(() => {
     WA.nav.openTab('https://www.wikipedia.org/');
            });
 export {}
