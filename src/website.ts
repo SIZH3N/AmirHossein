27f61c29e2764edd1df09ce19e487f2f1c70e2ea
AmirHossein <109079832+SIZH3N@users.noreply.github.com>
2024-09-23 13:24:34 +0330

    some changes that i don't remember

--- src/website.ts
+++ src/website.ts
@@ -1,7 +1,15 @@
+/// <reference types="@workadventure/iframe-api-typings" />
+
 WA.chat.sendChatMessage('Hello world', 'Mr Robot');
 console.log("the website script is open now");
 
-WA.room.onEnterLayer('chat').subscribe(() => {
-    WA.nav.openTab('https://www.wikipedia.org/');
-           });
+WA.onInit().then(() => {
+    WA.room.area.onEnter('chat').subscribe(() => {
+        WA.nav.openTab('https://www.wikipedia.org/');
+               });
+    console.log('Current player name: ', WA.player.name);
+});
+
+
+
 export {}
